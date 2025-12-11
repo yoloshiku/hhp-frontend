8052715faa39c09703453b311dc17010e97e0c39
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2025-12-11 21:07:17 +0200

    Removed bg test

--- src/pages/WhoWeServe.jsx
+++ src/pages/WhoWeServe.jsx
@@ -2,7 +2,7 @@ export default function WhoWeServe() {
   return (
     <div className="section">
       <div className="container">
-        <h1 className="bg-red-900">Who We Serve</h1>
+        <h1>Who We Serve</h1>
         <p>
           TODO: Recreate this page&apos;s content from WordPress using the HHP style guide.
         </p>
