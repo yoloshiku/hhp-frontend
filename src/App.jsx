df112b2ee1719516e59a04c36509f5d192e04341
ANN <gpreetham@MacBook-Air-174.local>
2026-02-02 18:59:18 -0500

    Implement Shared Patient Information flow with reports, assets, navigation, and scroll-to-top behavior fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles/global.css"
 import { AuthProvider } from "./context/AuthContext"
 import Header from "./layout/Header"
 import Footer from "./layout/Footer"
-
+import ScrollToTop from "./components/ScrollToTop";
 /* Pages */
 import Home from "./pages/Home"
 import MedicalDisclaimer from "./pages/MedicalDisclaimer"
@@ -30,93 +30,105 @@ import LearningAcademyMandarin from "./pages/LearningAcademyMandarin";
 import OnlineEvents from "./pages/OnlineEvents";
 import StopType2Diabetes from "./pages/StopType2Diabetes";
 import SharedPatientInformation from "./pages/SharedPatientInformation";
+import MigraineReports from "./pages/MigraineReports";
+import LupusReports from "./pages/LupusReports";
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <div className="app">
-          <Header />
+        <ScrollToTop />
+        <AuthProvider>
+          <div className="app">
+            <Header />
 
-          <main>
-            <Routes>
-              {/* Home */}
-              <Route path="/" element={<Home />} />
+            <main>
+              <Routes>
+                {/* Home */}
+                <Route path="/" element={<Home />} />
 
-              {/* Legal */}
-              <Route path="/medical-disclaimer" element={<MedicalDisclaimer />} />
+                {/* Legal */}
+                <Route path="/medical-disclaimer" element={<MedicalDisclaimer />} />
 
-              {/* Who We Are */}
-              <Route path="/who-we-are/about-us" element={<AboutUs />} />
-              <Route path="/who-we-are/meet-the-team" element={<MeetTheTeam />} />
+                {/* Who We Are */}
+                <Route path="/who-we-are/about-us" element={<AboutUs />} />
+                <Route path="/who-we-are/meet-the-team" element={<MeetTheTeam />} />
 
-              {/* Why HHP */}
-              <Route path="/why-hhp/who-we-serve" element={<WhoWeServe />} />
-              {/* What We Do */}
-              <Route
-                path="/what-we-do/healthcare-access/introduction"
-                element={<HealthcareAccessIntro />}
-              />
+                {/* Why HHP */}
+                <Route path="/why-hhp/who-we-serve" element={<WhoWeServe />} />
+                {/* What We Do */}
+                <Route
+                  path="/what-we-do/healthcare-access/introduction"
+                  element={<HealthcareAccessIntro />}
+                />
 
-              <Route
-                path="/what-we-do/patient-advocacy/northern-ireland"
-                element={<PatientAdvocacyNI />}
-              />
+                <Route
+                  path="/what-we-do/patient-advocacy/northern-ireland"
+                  element={<PatientAdvocacyNI />}
+                />
 
-              <Route
-                path="/what-we-do/patient-advocacy/los-angeles"
-                element={<PatientAdvocacyLA />}
-              />
+                <Route
+                  path="/what-we-do/patient-advocacy/los-angeles"
+                  element={<PatientAdvocacyLA />}
+                />
 
-              <Route path="/what-we-do/learning-academy/videos" element={<LearningAcademyVideos />} />
-              <Route path="/what-we-do/learning-academy/english" element={<LearningAcademyEnglish />} />
-              <Route path="/what-we-do/learning-academy/spanish" element={<LearningAcademySpanish />} />
-              <Route path="/what-we-do/learning-academy/mandarin" element={<LearningAcademyMandarin />} />
+                <Route path="/what-we-do/learning-academy/videos" element={<LearningAcademyVideos />} />
+                <Route path="/what-we-do/learning-academy/english" element={<LearningAcademyEnglish />} />
+                <Route path="/what-we-do/learning-academy/spanish" element={<LearningAcademySpanish />} />
+                <Route path="/what-we-do/learning-academy/mandarin" element={<LearningAcademyMandarin />} />
 
 
-              <Route
-                path="/what-we-do/online-events"
-                element={<OnlineEvents />}
-              />
+                <Route
+                  path="/what-we-do/online-events"
+                  element={<OnlineEvents />}
+                />
 
-              <Route
-                path="/what-we-do/stop-type-2-diabetes/introduction"
-                element={<StopType2Diabetes />}
-              />
+                <Route
+                  path="/what-we-do/stop-type-2-diabetes/introduction"
+                  element={<StopType2Diabetes />}
+                />
 
-              <Route
-                path="/what-we-do/shared-patient-information"
-                element={<SharedPatientInformation />}
-              />
+                <Route
+                  path="/what-we-do/shared-patient-information"
+                  element={<SharedPatientInformation />}
+                />
+                <Route
+                  path="/shared-patient-information/migraine"
+                  element={<MigraineReports />}
+                />
 
-              {/* How to Help */}
-              <Route path="/how-to-help/" element={<HowToHelp />} /> 
-              {/* <Route path="/how-to-help/support-hhp" element={<SupportHHP />} /> */}
-              {/* <Route path="/how-to-help/volunteer" element={<Volunteer />} /> */}
-              <Route path="/how-to-help/intern" element={<Intern />} />
-              <Route
-                path="/how-to-help/become-hhp-partner"
-                element={<BecomePartner />}
-              />
-              <Route
-                path="/how-to-help/matching-donations"
-                element={<MatchingDonations />}
-              />
+                <Route
+                  path="/shared-patient-information/lupus"
+                  element={<LupusReports />}
+                />
+                {/* How to Help */}
+                <Route path="/how-to-help/" element={<HowToHelp />} /> 
+                {/* <Route path="/how-to-help/support-hhp" element={<SupportHHP />} /> */}
+                {/* <Route path="/how-to-help/volunteer" element={<Volunteer />} /> */}
+                <Route path="/how-to-help/intern" element={<Intern />} />
+                <Route
+                  path="/how-to-help/become-hhp-partner"
+                  element={<BecomePartner />}
+                />
+                <Route
+                  path="/how-to-help/matching-donations"
+                  element={<MatchingDonations />}
+                />
 
-              {/* Auth */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
+                {/* Auth */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
 
-              {/* Donate */}
-              <Route path="/donate" element={<Donate />} />
-            </Routes>
-          </main>
+                {/* Donate */}
+                <Route path="/donate" element={<Donate />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </AuthProvider>
+            <Footer />
+          </div>
+        </AuthProvider>
     </Router>
   )
 }
 
 export default App
+
---
+++ src/pages/MigraineReports.jsx
@@ -0,0 +1,16 @@
+export default function MigraineReports() {
+  return (
+    <main className="spi-page">
+      <section className="spi-section">
+        <div className="spi-container">
+          <h1 className="spi-title">Migraine Reports</h1>
+
+          <p className="spi-body">
+            This page will contain Shared Patient Information reports related to
+            migraines. Content coming soon.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
